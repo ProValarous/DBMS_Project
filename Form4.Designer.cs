1dd80dcdeb2d8e45cabba9a155a31d204c7a4abd
Ahmed Atif <65737137+ProValarous@users.noreply.github.com>
2021-12-04 10:31:26 +0500

    gen ui update

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -38,11 +38,12 @@ namespace STORE_Improvised
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(207, 93);
+            this.textBox1.Location = new System.Drawing.Point(207, 133);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(459, 20);
             this.textBox1.TabIndex = 0;
@@ -63,7 +64,7 @@ namespace STORE_Improvised
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.GhostWhite;
-            this.label1.Location = new System.Drawing.Point(57, 91);
+            this.label1.Location = new System.Drawing.Point(57, 131);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(106, 21);
             this.label1.TabIndex = 2;
@@ -75,7 +76,7 @@ namespace STORE_Improvised
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.GhostWhite;
-            this.label2.Location = new System.Drawing.Point(57, 152);
+            this.label2.Location = new System.Drawing.Point(57, 192);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(146, 21);
             this.label2.TabIndex = 4;
@@ -84,7 +85,7 @@ namespace STORE_Improvised
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(207, 152);
+            this.textBox2.Location = new System.Drawing.Point(207, 192);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(459, 84);
@@ -96,7 +97,7 @@ namespace STORE_Improvised
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.GhostWhite;
-            this.label3.Location = new System.Drawing.Point(57, 253);
+            this.label3.Location = new System.Drawing.Point(57, 293);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(97, 21);
             this.label3.TabIndex = 6;
@@ -105,7 +106,7 @@ namespace STORE_Improvised
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(217, 257);
+            this.textBox3.Location = new System.Drawing.Point(217, 297);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(50, 20);
             this.textBox3.TabIndex = 5;
@@ -116,11 +117,23 @@ namespace STORE_Improvised
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.GhostWhite;
-            this.label4.Location = new System.Drawing.Point(201, 256);
+            this.label4.Location = new System.Drawing.Point(201, 296);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(19, 21);
             this.label4.TabIndex = 7;
             this.label4.Text = "$";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.GhostWhite;
+            this.label5.Location = new System.Drawing.Point(55, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 35);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Add Game";
             // 
             // Form4
             // 
@@ -129,6 +142,7 @@ namespace STORE_Improvised
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
@@ -154,5 +168,6 @@ namespace STORE_Improvised
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
